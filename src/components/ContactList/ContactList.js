2dc5cf0269431bcfff54dd07ec78a9bce872bd1f
Yefimkin Denys <iefimkindenis@gmail.com>
2022-11-02 17:50:38 +0200

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,52 +1,39 @@
 import PropTypes from "prop-types";
-// import { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchContacts } from 'redux/asyncThunk'
-// import { getContacts } from 'redux/contactsSlice';
-// import { getFilter } from 'redux/filterSlice';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/asyncThunk'
+import { getContacts } from 'redux/contactsSlice';
+import { getFilter } from 'redux/filterSlice';
 import style from './ContactList.module.css';
 import ContactItem from '../ContactItem/ContactItem';
 
-const ContactList = ({contacts}) => {
-    const elements = contacts.map(contact => <ContactItem key={contact.id} {...contact} />)
-    return (
-        <>
-            <ul className={style.li}>
-                {elements}
-            </ul>
-        </>
-    )
-}
+const ContactList = () => {
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
 
+    const {contacts} = useSelector(getContacts);
+    const filter = useSelector(getFilter);
 
-// const ContactList = () => {
-//     const dispatch = useDispatch();
+    const filteredContacts = () => {
+        if (!contacts) {
+            return;
+        }
+        return contacts.filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase())
+        );
+    }
 
-//     useEffect(() => {
-//         dispatch(fetchContacts());
-//     }, [dispatch]);
+    const filterContacts = filteredContacts();
 
-//     const {contacts} = useSelector(getContacts);
-//     const filter = useSelector(getFilter);
-
-//     const filteredContacts = () => {
-//         if (!contacts) {
-//             return;
-//         }
-//         return contacts.filter(contact =>
-//             contact.name.toLowerCase().includes(filter.toLowerCase())
-//         );
-//     }
-
-//     const filterContacts = filteredContacts();
-
-//     return <ul className={style.li}>
+    return <ul className={style.li}>
         
-//         {filterContacts.map(item =>
-//             <ContactItem key={item.id} data={item} />)}
-//         </ul>
-// }
+        {filterContacts.map(item =>
+            <ContactItem key={item.id} data={item} />)}
+        </ul>
+}
 
 export default ContactList;
 
